ea6b0b21f2a121eb0d9cf522f0eeea3d48f3c60c
王斐 <wangfeia@zhubajie.com>
2015-09-18 18:35:09 +0800

    添加一个JS文件,用于存放支持IE8的js

---
+++ day2/3.dom/js/supportIE8.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2015/9/18.
+ */
+//解决IE8之类不支持getElementsByClassName
+if (!document.getElementsByClassName) {
+    document.getElementsByClassName = function (className, element) {
+        var children = (element || document).getElementsByTagName('*');
+        var elements = new Array();
+        for (var i = 0; i < children.length; i++) {
+            var child = children[i];
+            var classNames = child.className.split(' ');
+            for (var j = 0; j < classNames.length; j++) {
+                if (classNames[j] == className) {
+                    elements.push(child);
+                    break;
+                }
+            }
+        }
+        return elements;
+    };
+}
